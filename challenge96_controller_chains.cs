43090e9b1ac2da959a956f103113b65099dbe0ab
Rob Wettlaufer <rdwettlaufer@gmail.com>
2013-11-17 18:21:59 -0500

    Update and rename challenge96_controller_chains.cs to Challenge96.cs

--- challenge96_controller_chains.cs
+++
@@ -1,8 +0,0 @@
-// http://www.reddit.com/r/dailyprogrammer/comments/zfe7v/9052012_challenge_96_easy_controller_chains/
-public int howManyPlayers(int money) {
-    if (money < controllerPrice){return players;}
-    dollars = money;
-    dollars = dollars - extraPlayer;
-    players = (int) ((dollars / fullTeam) * 4) + (dollars % fullTeam) / controllerPrice + 2;
-    return players;
-}
