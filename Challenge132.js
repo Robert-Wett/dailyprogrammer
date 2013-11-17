b3e58ea73f7adda3ebb83face8be692c47c9e81e
Rob Wettlaufer <rdwettlaufer@gmail.com>
2013-11-17 18:25:02 -0500

    Update Challenge132.js

--- Challenge132.js
+++ Challenge132.js
@@ -1,15 +1,15 @@
+// http://www.reddit.com/r/dailyprogrammer/comments/1hvh6u/070813_challenge_132_easy_greatest_common_divisor/
+
 /*
-The Greatest Common Divisor of a given set of integers is the greatest integer that 
-can divide these integers without any remainder. From Wikipedia, take a look at this 
-example: for the integers 8 and 12, the highest integer that divides them without 
-remainder is 4.
-
-Your goal is to write a program that takes two integers, and returns the greatest 
-common divisor. You may pick any algorithm or approach you prefer, though a good 
-starting point is Euclid's algorithm.
-Problem Author: nint22
-
-http://www.reddit.com/r/dailyprogrammer/comments/1hvh6u/070813_challenge_132_easy_greatest_common_divisor/
+    The Greatest Common Divisor of a given set of integers is the greatest integer that 
+    can divide these integers without any remainder. From Wikipedia, take a look at this 
+    example: for the integers 8 and 12, the highest integer that divides them without 
+    remainder is 4.
+    
+    Your goal is to write a program that takes two integers, and returns the greatest 
+    common divisor. You may pick any algorithm or approach you prefer, though a good 
+    starting point is Euclid's algorithm.
+    Problem Author: nint22
 */
 
 function gcd( highestNum, lowestNum ) {
