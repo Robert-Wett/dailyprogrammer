bff949f97ccd5f6043a5f80c73917d0d867ac8d5
Rob Wettlaufer <rdwettlaufer@gmail.com>
2013-11-17 18:24:27 -0500

    Update Challenge122.cs

--- Challenge122.cs
+++ Challenge122.cs
@@ -1,8 +1,8 @@
-/*
-Find words in a word list that contain all the vowels in alphabetical order, 
-non-repeated, where vowels are defined as A E I O U Y.
+// http://www.reddit.com/r/dailyprogrammer/comments/1aih0v/031813_challenge_122_easy_words_with_ordered/
 
-http://www.reddit.com/r/dailyprogrammer/comments/1aih0v/031813_challenge_122_easy_words_with_ordered/
+/*
+    Find words in a word list that contain all the vowels in alphabetical order, 
+    non-repeated, where vowels are defined as A E I O U Y.
 */
 
 static void Main(string[] args)
