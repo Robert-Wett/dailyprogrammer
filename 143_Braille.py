0b61b52240061f8209d65593533e0547e509bc7a
Rob Wettlaufer <rdwettlaufer@gmail.com>
2013-12-04 21:48:30 -0500

    Update 143_Braille.py

--- 143_Braille.py
+++ 143_Braille.py
@@ -39,82 +39,106 @@ import re
 f = open("files/braille.txt")
 raw_data = [x.strip().split(" ") for x in f.readlines()]
 
+alphabet = "abcdefghijklmnopqrstuvwxyz "
+space = "      "
+
 braille_data = \
 ["O.....", "O.O...", "OO....", "OO.O..", "O..O..", 
 "OOO...", "OOOO..", "O.OO..", ".OO...", ".OOO..",
 "O...O.", "O.O.O.", "OO..O.", "OO.OO.", "O..OO.",
 "OOO.O.", "OOOOO.", "O.OOO.", ".OO.O.", ".OOOO.",
 "O...OO", "O.O.OO", ".OOO.O", "OO..OO", "OO.OOO",
-"O..OOO", "......"]
+"O..OOO", space]
 
-braille_pound = \
-["#.....", "#.#...", "##....", "##.#..", "#..#..", 
-"###...", "####..", "#.##..", ".##...", ".###..",
-"#...#.", "#.#.#.", "##..#.", "##.##.", "#..##.",
-"###.#.", "#####.", "#.###.", ".##.#.", ".####.",
-"#...##", "#.#.##", ".###.#", "##..##", "##.###",
-"#..###", "......"]
-
-letter_lookup = {k:v for (k, v) in zip(braille_data, "abcdefghijklmnopqrstuvwxyz ")}
+letter_lookup = {k:v for (k, v) in zip(braille_data, alphabet)}
 togethered = "".join([letter_lookup["".join(x)] for x in list(zip(*raw_data))])
 print(togethered, end="\n\n")
 
 #--------------------------------------
 # Now we implement the reverse function
+#
+# This is me just messing around with something
+# and iterating over it to make it better. I'm 
+# really experimenting with args and kwargs and 
+# whatnot and what have you
 #--------------------------------------
 
 # Add a couple other lists to build a dictionary/lookup with
+# TODO: Generate lists based on a char for raised and a char
+#       for lowered.
+poundlabel="'#' for raised, '.' for lowered"
 braille_pound = \
 ["#.....", "#.#...", "##....", "##.#..", "#..#..", 
 "###...", "####..", "#.##..", ".##...", ".###..",
 "#...#.", "#.#.#.", "##..#.", "##.##.", "#..##.",
 "###.#.", "#####.", "#.###.", ".##.#.", ".####.",
 "#...##", "#.#.##", ".###.#", "##..##", "##.###",
-"#..###", "......"]
+"#..###", space]
 
+binlabel="'1' for raised and '0' for lowered"
 braille_binary = \
 ["100000", "101000", "110000", "110100", "100100", 
 "111000", "111100", "101100", "011000", "011100",
 "100010", "101010", "110010", "110110", "100110",
 "111010", "111110", "101110", "011010", "011110",
 "100011", "101011", "011101", "110011", "110111",
-"100111", "000000"]
+"100111", space]
 
-lookup = {k:v for (k, v) in zip("abcdefghijklmnopqrstuvwxyz ", braille_data)}
-lookup_p = {k:v for (k, v) in zip("abcdefghijklmnopqrstuvwxyz ", braille_pound)}
-lookup_b = {k:v for (k, v) in zip("abcdefghijklmnopqrstuvwxyz ", braille_binary)}
+lookup   = {k:v for (k, v) in zip(alphabet, braille_data)}
+lookup_p = {k:v for (k, v) in zip(alphabet, braille_pound)}
+lookup_b = {k:v for (k, v) in zip(alphabet, braille_binary)}
 
 def pad(string, length=2, delim=' '):
     """ Take a string and add a specified character every nth index """
-    return delim.join(string[i:i+length] for i in range(0,len(string),length))
+    return delim+delim.join(string[i:i+length] for i in range(0,len(string),length))+delim
 
-def get_braille(sentence, lookup=lookup, delimchar=' '):
-    """This takes a string and returns the English Braille
-       representation in three lines of text. It supports
-       [aA-zZ] and spaces. It assumes the lookups passed
-       or built already."""
+def get_braille(sentence, *args, map=lookup, delimchar=' ', lname="'O' for raised and '.' for lowered", ljust=True):
+#    """This takes a string and returns the English Braille
+#       representation in three lines of text. It supports
+#       [aA-zZ] and spaces. It assumes the lookups passed
+#       or built already."""
     
-    r1, r2, r3 = [], [], []
+    r1, r2, r3, r4 = [], [], [], []
     for c in sentence.lower():
-       translated = re.findall('..', lookup[c])
-       r1+=translated[0]
-       r2+=translated[1]
-       r3+=translated[2]
-    print("Brailling '", sentence, "':", sep='')
+       if ljust:
+           r1+=c+" "
+       else:
+           r1+=" "+c
+       translated = re.findall('..', map[c])
+       r2+=translated[0]
+       r3+=translated[1]
+       r4+=translated[2]
+    print("Translating '", sentence, "' to braille, with ", lname, sep='')
+    if args:
+        details = ["".join(x) for x in args]
+        for line in details:
+            print(line)
     print("{0:3}".format(pad("".join(r1), delim=delimchar)), 
           "{0:3}".format(pad("".join(r2), delim=delimchar)), 
           "{0:3}".format(pad("".join(r3), delim=delimchar)), 
+          "{0:3}".format(pad("".join(r4), delim=delimchar)), 
           sep="\n", end="\n\n")
 
+# Demonstrate the different usages
 get_braille("Hello World")
-get_braille("Hello World", lookup_p)
-get_braille("Hello World", lookup_b)
+get_braille("Hello World", map=lookup_p, lname=poundlabel)
+get_braille("Hello World", map=lookup_b, lname=binlabel)
 get_braille("Rob Wett")
-get_braille("Rob Wett", lookup_p)
-get_braille("Rob Wett", lookup_b, '+')
+get_braille("Rob Wett", map=lookup_p, lname=poundlabel)
+get_braille("Rob Wett", "Using '|' as a delimiting character: ",
+           "Lets add another line, just args stuff an all", 
+           map=lookup_p, lname=poundlabel, delimchar='|')
+get_braille("Rob Wett", "Using '+' as a delimiting character: ", 
+            map=lookup_b, lname=binlabel, delimchar='+')
+get_braille("Rob Wett", "Using '-' as a delimiting character: ", 
+            map=lookup_b, lname=binlabel, delimchar='-')
+get_braille("Rob Wett", "Using '-' as a delimiting character: ", 
+            map=lookup_b, lname=binlabel, delimchar='-', ljust=False)
 get_braille("braille")
-get_braille("braille", lookup_p)
-get_braille("braille", lookup_b)
-get_braille("j is nonsense")
-get_braille("j is nonsense", lookup_p)
-get_braille("j is nonsense", lookup_b)
+# messin
+messin_args="library\ncards\nr\n4\ntards"
+get_braille("braille", messin_args, map=lookup_p, lname=poundlabel)
+get_braille("braille", map=lookup_b, lname=binlabel)
+get_braille("j is nonsense", messin_args)
+get_braille("j is nonsense", map=lookup_p, lname=poundlabel, ljust=False)
+get_braille("j is nonsense", map=lookup_b, lname=binlabel)
