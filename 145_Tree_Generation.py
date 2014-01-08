f772e3878fe691b4b122b645377b97fb3a8516ca
Rob Wettlaufer <rdwettlaufer@gmail.com>
2014-01-07 22:40:23 -0500

    Create 145_Tree_Generation.py

---
+++ 145_Tree_Generation.py
@@ -0,0 +1,14 @@
+# http://www.reddit.com/r/dailyprogrammer/comments/1t0r09/121613_challenge_145_easy_tree_generation/
+import sys
+
+
+levels, trunk, leaves = sys.argv[1].split(" ")
+levels = int(levels)
+top = levels // 2 + 1
+for x in range(top):
+    pad = " "*(top-x)
+    print(pad+(leaves*(x * 2 + 1))+pad)
+pad = " "*((levels - 3)//2)
+print(pad+" "+trunk*3+pad)
+
+// http://ideone.com/JUy4Mu
